7f12219d273b6e73b873d6178d5080fd923ceba9
Максим <lonelymaks123@gmail.com>
2026-03-26 22:32:15 +0200

    newHw8

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,95 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello SkyPro!");
         // Задание номер 1
+        System.out.println("Задание номер 1 часть 1");
+        int[] agg = new int[12];
+        agg[0] = 0;
+        for (int i = 0; i < agg.length; i++) {
+            System.out.println(agg[0]);
 
         }
+        System.out.println("Задание 1 часть 2");
+
+
+        int[] weight = new int[3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+        for (int i = 0; i < weight.length; i++) {
+            System.out.println(weight[0]);
+            System.out.println(weight[1]);
+            System.out.println(weight[2]);
+            break;
+
+        }
+        System.out.println("Задание номер 1 часть 3");
+        double[] free = {1.75, 2.57, 6.987};
+        for (int i = 0; i < free.length; i++) {
+            System.out.println(free[0]);
+            System.out.println(free[1]);
+            System.out.println(free[2]);
+            break;
+
+        }
+        // Задание номер 2
+        System.out.println("Задание номер 2 ");
+        int[] agg1 = new int[12];
+        for (int i = 0; i < agg1.length; i++) {
+            agg1[i] = i + 1;
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(agg1[i]);
+        }
+        System.out.println();
+        int[] weight1 = new int[3];
+        for (int i = 0; i < weight1.length; i++) {
+            weight1[i] = i + 1;
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(weight1[i]);
+        }
+        System.out.println();
+        double[] free1 = {1.75, 2.57, 6.987};
+        free1[0] = 1.75;
+        free1[1] = 2.57;
+        free1[2] = 6.987;
+        for (int i = 0; i < free1.length; i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(free1[i]);
+        }
+        System.out.println();
+
+        //Задание номер 3
+        System.out.println("Задание номер 3");
+        for (int i = weight.length - 1; i >= 0; i--) {
+            System.out.print(weight[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        //Задача номер 4
+        System.out.println("Задание номер 4 ");
+        int[] bananas = {1, 3, 5, 7, 9};
+        for (int j = 0; j < bananas.length; j++) {
+            if (bananas[j] % 2 != 0) {
+                bananas[j] = bananas[j] + 1;
+            }
+            System.out.print(bananas[j] + " ");
+        }
+
 
     }
+
+
+}
+
+
 
